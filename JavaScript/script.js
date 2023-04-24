df743f3ec661557c3e7cf6ebbf37d84d77e72a26
zerohero <jafethunu@gmail.com>
2023-04-24 00:47:53 -0600

    Agregar archivos de css y javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,5 @@
+const titulo = document.querySelector('h1');
+const parrafo = document.querySelector('p');
+const parrafo2 = document.querySelector ('.parrafo');
+const parrafo3 = document.querySelector ('#parrafito');
+const input = document.querySelector ('input');
