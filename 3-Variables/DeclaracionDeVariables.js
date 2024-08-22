a18530401bc89f88e54fff77b8ad19bc9ea9aaac
Jafeth Jesus <JafethUnu@gmail.com>
2024-08-22 11:49:42 -0600

    Declaracion y uso de variables

---
+++ 3-Variables/DeclaracionDeVariables.js
@@ -0,0 +1,16 @@
+// Debe de comenzar una letra en minuscula y la segunda palabra en mayuscula
+
+var miNumero1 = 15;
+console.log(miNumero1);
+
+let miVariable2 = "Domingo";
+console.log(miVariable2);
+console.log(typeof miVariable2); // typeof es una palabra reservada para saber el tipo de variable
+
+// Hosting
+var x; // Declarar la variable
+x = 12; // Inicializar la variable
+console.log(x);
+
+// Constantes 
+const SEGUNDOS_POR_MINUTO = 60;
