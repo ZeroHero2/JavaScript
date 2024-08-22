c9bf9fdbba8235bfa7f888281fad2a961fa41774
JAFETH JESUS SANCHEZ SANCHEZ <jafeth.sanchez@senado.gob.mx>
2024-08-22 10:01:47 -0600

    Creacion de repo con js y funciones

---
+++ funcion1.js
@@ -0,0 +1,24 @@
+
+// Declarar una funcion
+function suma (x, y){
+    return x*y;
+}
+
+console.log(suma(13,98));
+
+
+// Declarar una funcion 
+function calcularDescuento (precio, descuentoPorcentaje){
+    const descuento = (precio * descuentoPorcentaje) / 100;
+    const precioConDescuento = precio - descuento;
+    
+    return precioConDescuento;
+    }
+    
+    const precio = 100;
+    const descuentoPorcentaje = 20;
+    const calcularPrecioDescuento = (precio - descuentoPorcentaje)
+    
+    console.log("El precio original es: " + precio);
+    console.log("El descuento original es: " + descuentoPorcentaje + "%");
+    console.log("El precio con descuento: " + calcularPrecioDescuento)
