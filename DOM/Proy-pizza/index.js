ee84c006b15fa5b435b2908921f287efb6aa4f70
Jafeth Jesus <JafethUnu@gmail.com>
2024-08-28 18:55:36 -0600

    aplicacion de etiquetas en js para imprimir por consola

---
+++ DOM/Proy-pizza/index.js
@@ -0,0 +1,8 @@
+// Obtener elemento por el id
+// .getElementByid()
+const contenedor = document.getElementById('contenedor');
+console.log(contenedor.innerHTML); // Se accede a la estructura HTML de esa etiqueta
+// Crear variable texto
+const titulo = document.getElementById('titulo');
+console.log (titulo.innerText); // Se devuelve el texto de la etiqueta inner = interno 
+console.log (titulo.tagName); // Devuelve la etiqueta 
