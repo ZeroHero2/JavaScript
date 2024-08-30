7c55861c7bc2cc33602e5679fc86fbddfeeb0b95
Jafeth Jesus <JafethUnu@gmail.com>
2024-08-30 15:35:31 -0600

    uso de diferentes get element

--- DOM/Proy-pizza/index.js
+++ DOM/Proy-pizza/index.js
@@ -1,3 +1,4 @@
+// -------- Obtener elemento en base a su ID ------------
 // Obtener elemento por el id
 // .getElementByid()
 const contenedor = document.getElementById('contenedor');
@@ -6,3 +7,16 @@ console.log(contenedor.innerHTML); // Se accede a la estructura HTML de esa etiq
 const titulo = document.getElementById('titulo');
 console.log (titulo.innerText); // Se devuelve el texto de la etiqueta inner = interno 
 console.log (titulo.tagName); // Devuelve la etiqueta 
+
+// -------- Obtener elemento en base a su clase ------------
+const toppings = document.getElementsByClassName('topping');
+console.log(toppings);
+console.log(toppings[3]); // Acceder a un elemento especifico
+
+// -------- Obtener elemento que tenga una etiqueta en especifico ------------
+let misToppings = document.getElementsByTagName('li');
+console.log(misToppings);
+
+// -------- Seleccionar un elemento en especifico  ------------
+let cebolla = document.querySelector('#cebolla');
+console.log(cebolla);
