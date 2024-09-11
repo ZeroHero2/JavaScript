b411532529d7c42262cc47e3d0ec7ec19ebef519
Jafeth Jesus <JafethUnu@gmail.com>
2024-09-11 10:31:46 -0600

    Manejo de arrays y sus metodos

---
+++ Arrays/Array.js
@@ -0,0 +1,15 @@
+let frutas = ['manzana', 'pera', 'platano','mango','sandia','kiwi'];
+console.log(frutas);
+console.log(frutas.length);
+
+// Agrega un elemento
+let otraFruta = frutas.push('pepino');
+console.log(frutas);
+
+// Elimina el ultimo elemento
+let ultimo = frutas.pop();
+console.log(frutas);
+
+// Elimina el primer elemento del array 
+let primero = frutas.shift();
+console.log(frutas);
